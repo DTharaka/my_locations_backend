21ede5807d4968360da58a1d6f67cb6c180abdc7
DTharaka <tharakadil22@gmail.com>
2020-10-25 03:56:54 +0530

    added more route and some validations

---
+++ controllers/users-controllers.js
@@ -0,0 +1,52 @@
+const bodyParser = require('body-parser');
+const { v4: uuidv4 } = require('uuid');
+
+const HttpError = require('../models/http-error');
+
+const DUMMY_USERS= [
+    {
+        id: 'u1', 
+        name: 'test',
+        email: 'test@test.com', 
+        password: 'test@123',
+    }
+];
+
+const getUsers = (req,res,next) => {
+    res.status(200).json({users: DUMMY_USERS})
+};
+
+const loginUser = (req,res,next) => {
+    const { email, password } = req.body;
+
+    const identifiedUser = DUMMY_USERS.find((p) => {u.email === email});
+
+    if (!identifiedUser || identifiedUser.id !== password) {
+        throw new HttpError('Could not identify user !', 401);
+    }
+
+    res.json({message: 'Login Successful !'});
+};
+
+const signUpUsers = (req,res,next) => {
+    const { name, email, password } = req.body;
+
+    const hasUsers = DUMMY_USERS.find((u) => {u.email === email});
+    if (hasUsers) {
+        throw new HttpError('Email already exists !', 422);
+    }
+
+    const createdUser = {
+        id: uuidv4(),
+        name: name,
+        email: email,
+        password: password
+    }
+
+    DUMMY_USERS.push(createdUser);
+    res.status(201).json({user: createdUser});
+};
+
+exports.getUsers = getUsers;
+exports.loginUser = loginUser;
+exports.signUpUsers = signUpUsers;
